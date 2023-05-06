41077122104955a4802c15e7c05992baa7a92418
Ubuntu <ubuntu@ip-172-31-24-229.ap-southeast-1.compute.internal>
2023-05-06 02:24:39 +0000

    initial

---
+++ scan-images.sh
@@ -0,0 +1,21 @@
+image='harbor.alson.space/demo/sample-app:v1.3'
+user='1761693e-e8a7-48b2-953f-a68dc442c9f1'
+password='BJP5MWXm+aAe/WOaoyrVAiBAGiM='
+cwp_url='https://asia-northeast1.cloud.twistlock.com/japan-1167259786'
+
+result_url=$(./twistcli images scan --address $cwp_url --user $user --password $password $image | tail -1 | cut -c 33-)
+image_id=${result_url: -73}
+
+login=$(curl \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d '{"username": "1761693e-e8a7-48b2-953f-a68dc442c9f1", "password": "BJP5MWXm+aAe/WOaoyrVAiBAGiM=" }' \
+  $cwp_url/api/v22.12/authenticate)
+
+token=$(echo $login|jq .token)
+token=${token:1:-1}
+
+pcs_url="https://asia-northeast1.cloud.twistlock.com/japan-1167259786/api/v1/scans?limit=17&offset=0&project=Central+Console&reverse=true&search=$image_id&sort=entityInfo.vulnerabilityRiskScore&type=ciImage,ciTas"
+
+scan_result=$(curl -o response.json -H "Authorization: Bearer $token" -X GET $pcs_url)
+python3 scan-result.py
---
+++ scan-result.py
@@ -0,0 +1,27 @@
+import json
+import pandas as pd
+from pandas import json_normalize
+from tabulate import tabulate
+
+with open('response.json') as user_file:
+  file_contents = user_file.read()
+
+parsed_json = json.loads(file_contents)
+vul_info = parsed_json[0]['entityInfo']['vulnerabilityDistribution']
+vul_info_df = json_normalize(parsed_json[0]['entityInfo']['vulnerabilityDistribution'])
+
+print("-----Extracting information from API-----")
+print(vul_info_df.to_string(index=False))
+
+binary_info = parsed_json[0]['entityInfo']['vulnerabilities']
+binary_info_info_df = json_normalize(parsed_json[0]['entityInfo']['vulnerabilities'])[['packageName','packageVersion','cve','severity','cvss']]
+#binary_info_info_df = binary_info_df.loc[binary_info_info_df['cveCount'] > 0 ]
+#['text', 'id', 'severity', 'cvss', 'status', 'cve', 'cause', 'description', 'title', 'vecStr', 'exploit', 'link', 'type', 'packageName', 'packageVersion', 'layerTime', 'templates', 'twistlock', 'cri', 'published', 'fixDate', 'applicableRules', 'discovered', 'binaryPkgs', 'functionLayer', 'exploits']
+
+print()
+print("-----Extracting Detail Information-----")
+#print(list(binary_info_info_df.columns.values))
+#print(binary_info_info_df.to_string(index=False))
+print(tabulate(binary_info_info_df, headers = 'keys', tablefmt = 'psql'))
+
+
