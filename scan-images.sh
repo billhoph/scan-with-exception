dd44ec5948c35db1ae24e3e2569a988fb76bc665
Ubuntu <ubuntu@ip-172-31-24-229.ap-southeast-1.compute.internal>
2023-05-08 04:50:39 +0000

    optimize scripts

--- scan-images.sh
+++ scan-images.sh
@@ -1,7 +1,18 @@
-image='harbor.alson.space/demo/sample-app:v1.3'
+#image='harbor.alson.space/demo/sample-app:v1.3'
+image=$1
 user='1761693e-e8a7-48b2-953f-a68dc442c9f1'
 password='BJP5MWXm+aAe/WOaoyrVAiBAGiM='
 cwp_url='https://asia-northeast1.cloud.twistlock.com/japan-1167259786'
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "username": "$user",
+  "password": "$password"
+}
+EOF
+}
 
 result_url=$(./twistcli images scan --address $cwp_url --user $user --password $password $image | tail -1 | cut -c 33-)
 image_id=${result_url: -73}
@@ -9,7 +20,7 @@ image_id=${result_url: -73}
 login=$(curl \
   -H "Content-Type: application/json" \
   -X POST \
-  -d '{"username": "1761693e-e8a7-48b2-953f-a68dc442c9f1", "password": "BJP5MWXm+aAe/WOaoyrVAiBAGiM=" }' \
+  -d "$(generate_post_data)" \
   $cwp_url/api/v22.12/authenticate)
 
 token=$(echo $login|jq .token)
